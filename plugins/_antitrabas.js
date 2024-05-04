b24f4601583ef1c8b8bd9d8ded5c85d07bb9b315
Dipak-69 <116695174+Deepak-69@users.noreply.github.com>
2024-05-04 07:59:44 +0530

    𓆩✮͢𝘿𝙞𝙥𝙖𝙠 𝙎𝙝𝙖𝙧𝙢𝙖𓆪

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -17,10 +17,10 @@ let bang = m.key.id
 let name = await conn.getName(m.sender)
 const user = `@${m.sender.split`@`[0]}`;
 let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": 'ɢᴀᴛᴀʙᴏᴛ-ᴍᴅ', 'jpegThumbnail': null}}}
-if (chat.antiTraba && m.text.length > 4000) { //Cantidad máxima de caracteres aceptados en un mensaje//
+if (chat.antiTraba && m.text.length > 4000) { //Maximum number of characters accepted in a message//
 if (isAdmin) return conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoAG']()} ${mid.smsAntiTraba(m)}`, mentions: [m.sender] }, { quoted: fakemek })
 if (!isBotAdmin) return conn.sendMessage(m.chat, {text: mid.smsAntiTraba2 + `\n${listAdmin}\n\n${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, {quoted: m})
-//await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[ ᴅᴇsᴀᴄᴛɪᴠᴀ ᴀɴᴛɪᴛʀᴀʙᴀ ]', usedPrefix+'apagar antitraba'], fakemek )
+//await conn.sendButton(m.chat, `*[ ! ] A message containing many characters was detected [ ! ]*\n`, `${isBotAdmin ? '' : 'I am not an administrator, I can not do anything :/'}`, author, ['[ ᴅᴇsᴀᴄᴛɪᴠᴀ ᴀɴᴛɪᴛʀᴀʙᴀ ]', usedPrefix+'apagar antitraba'], fakemek )
 if (isBotAdmin) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 setTimeout(() => {   
