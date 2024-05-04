f58f1fc48180fe2c3b98bf37c0105b34510c74b8
Dipak-69 <116695174+Deepak-69@users.noreply.github.com>
2024-05-04 10:31:41 +0530

    𓆩✮͢𝘿𝙞𝙥𝙖𝙠 𝙎𝙝𝙖𝙧𝙢𝙖𓆪

--- plugins/fun-adivinar_resp.js
+++ plugins/fun-adivinar_resp.js
@@ -5,26 +5,26 @@ const threshold = 0.72
 let handler = m => m 
 handler.before = async function (m) {
 
-//----------------------[adivina acertijo]----------------------
+//----------------------[guess]----------------------
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('That riddle it's already over!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tekateki[id][2];
-      m.reply(`*Respuesta correcta!*\n+${this.tekateki[id][2]} Exp`);
+      m.reply(`*Answer correct!*\n+${this.tekateki[id][2]} Exp`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`You almost did it!`);
+    else m.reply('Answer incorrect!');
 }
 
-///----------------------[adivina la canción]----------------------
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return !0;
+///----------------------[guess the song]----------------------
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/GUESS THE TITLE OF THE SONG/i.test(m.quoted.text)) return !0;
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {};
-    if (!(id in this.tebaklagu)) return m.reply('El juego ha terminado');
+    if (!(id in this.tebaklagu)) return m.reply('The game has finished');
     if (m.quoted.id == this.tebaklagu[id][0].id) {
       const json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]));
       if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
@@ -32,8 +32,8 @@ handler.before = async function (m) {
         m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} XP`);
         clearTimeout(this.tebaklagu[id][3]);
         delete this.tebaklagu[id];
-      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`);
-      else m.reply(`❌Incorrecto!`);
+      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Cashiers!`);
+      else m.reply(`❌Incorrect!`);
     }
     return !0
 }
